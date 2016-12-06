e5ef80a2c32df78639674ea0ac4b9e1196a4e68c
Maxim Lukoshko <www.maxim.by97@gmail.com>
2016-12-06 22:44:58 +0300

    Реализован поиск по маске

--- GenTree/DatabaseModel/Database/Database.cs
+++ GenTree/DatabaseModel/Database/Database.cs
@@ -29,7 +29,26 @@ namespace DatabaseModel.Database
 
         public IDictionary<Int32, Person> GetPeople(Person mask)
         {
-            throw new NotImplementedException();
+            IDictionary<Int32, Person> ret = new Dictionary<Int32, Person>();
+
+            foreach(Person person in data.Values)
+            {
+                if (mask.FirstName.Length > 0 && person.FirstName.IndexOf(mask.FirstName) < 0)
+                    continue;
+                if (mask.SecondName.Length > 0 && person.SecondName.IndexOf(mask.SecondName) < 0)
+                    continue;
+                if (mask.MiddleName.Length > 0 && person.MiddleName.IndexOf(mask.MiddleName) < 0)
+                    continue;
+                if (mask.BirthPlace.Length > 0 && person.BirthPlace.IndexOf(mask.BirthPlace) < 0)
+                    continue;
+                if (mask.IsCorrectDate && person.BirthDate != mask.BirthDate)
+                    continue;
+                
+                //Если всё совпало с маской, то добавляем человека
+                ret.Add(person.Code, person);
+            }
+
+            return ret;
         }
 
         public IDictionary<Int32, Person> GetPeopleByParentCode(Int32 parentCode)

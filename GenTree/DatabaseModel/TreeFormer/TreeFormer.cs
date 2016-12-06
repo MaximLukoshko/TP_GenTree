ab3c2f879d33dbfab8d51f0640d3b28a48c15c90
Maxim Lukoshko <www.maxim.by97@gmail.com>
2016-12-06 21:03:53 +0300

    Разработан класс TreeFormer

--- GenTree/DatabaseModel/TreeFormer/TreeFormer.cs
+++ GenTree/DatabaseModel/TreeFormer/TreeFormer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data;
+using DatabaseModel.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace DatabaseModel.TreeFormer
 {
-    class TreeFormer
+    class TreeFormer : ITreeFormer
     {
+        protected IDatabase database;
+        public TreeFormer(IDatabase dtBase)
+        {
+            if (null == dtBase)
+                throw new NullReferenceException();
+
+            database = dtBase;
+        }
+
+        public IDictionary<Int32, Person> FormTree(Int32 code)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
